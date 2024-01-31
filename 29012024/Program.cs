364d84df553514450be9f8d13abe4b652dbaf536
Vitaly Yarnov <vyarnov@gmail.com>
2024-01-31 12:36:19 +0300

    add task3 and task4 to HM to 29012024

--- 29012024/Program.cs
+++ 29012024/Program.cs
@@ -1,30 +1,30 @@
 ﻿﻿// Домашняя работа
 
-// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// Задача 1
 {
-    Console.WriteLine("Задача 1");
+    Console.WriteLine("Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.");
 }
-int number = 75;
+int numberHW1 = 75;
 int first = 7;
 int second = 23;
 
-if (number % first == 0 && number % second == 0)
+if (numberHW1 % first == 0 && numberHW1 % second == 0)
 {
-    Console.WriteLine("Число" + " " + number + " " + "кратно" + " " + first + " " + "и" + " " + second);
+    Console.WriteLine("Число" + " " + numberHW1 + " " + "кратно" + " " + first + " " + "и" + " " + second);
 }
 else
 {
-    Console.WriteLine("Число" + " " + number + " " + "НЕ кратно" + " " + first + " " + "и" + " " + second);
+    Console.WriteLine("Число" + " " + numberHW1 + " " + "НЕ кратно" + " " + first + " " + "и" + " " + second);
 }
 {
     Console.WriteLine("");
 }
-// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+// Задача 2
 
 {
-    Console.WriteLine("Задача 2");
+    Console.WriteLine("Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.");
 }
-int xCoordinate = 0;
+int xCoordinate = 1;
 int yCoordinate = -3;
 if (xCoordinate >= 1 && yCoordinate >= 1)
 {
@@ -49,6 +49,52 @@ else
 {
     Console.WriteLine("");
 }
+
+// Задача 3
+{
+    Console.WriteLine("Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.");
+}
+int numberHW3 = 69;
+int firstHW3 = numberHW3 / 10;
+int secondHW3 = numberHW3 % 10;
+if (firstHW3 > secondHW3)
+{
+    Console.WriteLine("В числе" + " " + numberHW3 + " " + "наибольшая цифра" + " " + firstHW3);
+}
+else
+{
+    Console.WriteLine("В числе" + " " + numberHW3 + " " + "наибольшая цифра" + " " + secondHW3);
+}
+{
+    Console.WriteLine("");
+}
+
+// Задача 4
+{
+    Console.WriteLine("Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.");
+}
+int numberHW4 = 697;
+int resualtHW4 = numberHW4;
+ while (numberHW4 > 0)
+{
+resualtHW4 = numberHW4 % 10; //- получаешь самую правую цифру числа
+Console.Write(resualtHW4 + ",");
+numberHW4 = numberHW4 / 10; // удаляешь самую правую цифру из числа num.
+//Console.WriteLine(numberHW4);
+}
+
+{
+    Console.WriteLine("");
+}
+
+
+
+
+// num % 10 - получаешь самую правую цифру числа num
+// num = num // 10 - удаляешь самую правую цифру из числа num.
+
+
+// // $"Текст {result} текст {a+100} текст {a - 500}"
 
 
 

55e7d4d685087802a7ad61e4c860a6ccac48183d
Vitaly Yarnov <vyarnov@gmail.com>
2024-02-14 16:29:38 +0300

    add text HomeWork to 08022024

--- 08022024/Program.cs
+++ 08022024/Program.cs
@@ -1,4 +1,74 @@
-﻿// Задайте двумерный массив. Найдите элементы, у которых оба
+﻿// Homework
+Console.Clear();
+int[,] array = new int[,] {
+    {1, 2, 3},
+    {1, 10, 0},
+    {7, 8, 2},
+    {9, 10, 11}
+};
+
+int[] SumRows(int[,] array)
+{
+    int[] arr = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++) // строки
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            sum += array[i, j];
+        }
+        arr[i] = sum;
+    }
+    return arr;
+}
+
+int MinIndex(int[] array)
+{
+    int mindex = 0;
+    for (int i = 0; i < array.Length; i++) // строки
+        if (array[i] < array[mindex])
+        {
+            mindex = i;
+        }
+    return mindex;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // стр
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write($"{array[i, j]}\t"); // "\t" = 4 пробела 
+        } // Закончили обход 0 строки, идем в 1
+        Console.WriteLine(); // Перенос на новую строчку
+    }
+}
+
+
+int[] ter = SumRows(array);
+// Console.WriteLine($"Массив: [ {string.Join("; ", ter)} ]");
+PrintArray(array);
+int mindx = MinIndex(ter);
+Console.WriteLine("Индекс строки массива с минимальной суммой элементов: " + mindx);
+
+//  for (int i = 0; i < array.GetLength(0); i++)
+//        {
+//         if (i + 1 == x)
+//           {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//              {
+//              if (j + 1 == y)
+//               result = array [i,j]; 
+//              }
+//           }
+
+
+
+
+
+
+// Задайте двумерный массив. Найдите элементы, у которых оба
 // индекса чётные, и замените эти элементы на их квадраты.
 // int[,] CreateMatrix(int rows, int columns)
 // {
@@ -17,14 +87,19 @@
 
 // void PrintMatrix(int[,] matr)
 // {
-//     for (int i = 0; i < matr.GetLength(0); i++) // стр
+// int[] arr = [];
+// for (int i = 0; i < array.GetLength(0); i++) // строки
+// {
+//     int sum = 0;
+//     for (int j = 0; j < array.GetLength(1); i++) // строки
 //     {
-//         for (int j = 0; j < matr.GetLength(1); j++) // столбцы
-//         {
-//             Console.Write($"{matr[i, j]}\t"); // "\t" = 4 пробела 
-//         } // Закончили обход 0 строки, идем в 1
-//         Console.WriteLine(); // Перенос на новую строчку
-//     }
+//         sum += array[i, j]
+//         }
+//     arr[i] = sum;
+// }
+
+// return arr;
+
 // }
 
 // void SquareElements(int[,] matr)

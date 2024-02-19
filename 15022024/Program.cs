f8cd2dc8dfd9a8e7a6a57d37c9d88934b39ac589
Vitaly Yarnov <vyarnov@gmail.com>
2024-02-19 12:04:04 +0300

    add full homework to 15022024

--- 15022024/Program.cs
+++ 15022024/Program.cs
@@ -2,72 +2,131 @@
 
 // Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
+// void PrintValues(int M, int N)
+// {
+//     if (M <= N)
+//     {
+//         if (M == N)
+//         {
+//             Console.WriteLine(M);
+//             return;
+//         }
+//         Console.WriteLine(M);
+//         PrintValues(M + 1, N);
+//     }
+//     if (M >= N)
+//     {
+//         if (M == N)
+//         {
+//             Console.WriteLine(N);
+//             return;
+//         }
+//         Console.WriteLine(M);
+//         PrintValues(M - 1, N);
+//     }
+// }
 
+// int CheckNaturNumb(int A)
+// {
+
+//     if (A <= 0)
+//     {
+//         PrintErrorNaturNumb();
+//     }
+
+//     return A;
+// }
+
+// void PrintErrorNaturNumb()
+// {
+//     Console.WriteLine("Вы ввели не натуральныое число");
+//     Console.WriteLine("Программа завершена");
+//     Environment.Exit(-1);
+// }
+
+
+// Console.WriteLine("Программа выведет все натуральные числа в промежутке от M до N:");
+// Console.WriteLine();
+
+
+// Console.Write("Введите начальное значение: ");
+// int valueM = Convert.ToInt32(Console.ReadLine());
+// valueM = CheckNaturNumb(valueM);
+
+// Console.Write("Введите конечное значение: ");
+// int valueN = Convert.ToInt32(Console.ReadLine());
+// valueN = CheckNaturNumb(valueN);
+
+// PrintValues(valueM, valueN);
 
 
 
 
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+
+// int CheckNegNumb(int A)
+// {
+
+//     if (A < 0)
+//     {
+//         PrintErrorNegNumb();
+//     }
+
+//     return A;
+// }
+
+// void PrintErrorNegNumb()
+// {
+//     Console.WriteLine("Вы ввели отрицательное число");
+//     Console.WriteLine("Программа завершена");
+//     Environment.Exit(-1);
+// }
+
+// int GetAkkerman(int M, int N)
+// {
+//     if (M == 0) return N + 1;
+
+//     else if (M > 0 && N == 0) return GetAkkerman(M - 1, 1);
+//     else return GetAkkerman(M - 1, GetAkkerman(M, N - 1));
+// }
+
+// Console.WriteLine("Программа вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа M до N:");
+// Console.WriteLine();
+
+// Console.Write("Введите значение M: ");
+// int valueM = Convert.ToInt32(Console.ReadLine());
+// valueM = CheckNegNumb(valueM);
+
+// Console.Write("Введите значение N: ");
+// int valueN = Convert.ToInt32(Console.ReadLine());
+// valueN = CheckNegNumb(valueN);
+
+
+// Console.Write($"Функция Аккермана равна: {GetAkkerman(valueM, valueN)}");
+
+
+
 // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Рекурсия - вызов метода внутри метода
-// Факториал - произ-е чисел от 1 до N включительно 
-// N -> 1; 4 * 3 * 2 * 1 = 24; N = 4
-
-// int FindFactorial (int n)
+// void PrintInvertedArray(char[] array, int J)
 // {
-//     // Базовый случай - случай остановки рекурсии
-//     if (n == 1) return 1;
-//     // Рекурсивный случай - случай вызова метода внутри метода
-//     return n * FindFactorial(n - 1);
-// }
-// Console.WriteLine(FindFactorial(5)); // n = 5
-// // 5 * 4 * 3 * 2 * 1 = 120
 
-// Напишите программу, которая будет принимать 
-// на вход число и  возвращать сумму его цифр.
-// Пример
-// 123 => 6 (1+2+3 = 6)
-// 63 => 9
-
-// int SumOfDigits (int number)
-// {
-//     // Базовый случай
-//     if (number == 0) return 0; // 3 + 6 + 0 = 9
-//     int result = number % 10 + SumOfDigits(number / 10);
-//     return result;
-// }
-
-// Console.WriteLine(SumOfDigits(63));
-
-// Задайте значение N. Напишите программу, которая выведет
-// все натуральные числа в промежутке от 1 до N.
-// Указание
-// Использовать рекурсию. Не использовать цикл.
-// Пример
-// N=5 => 1 2 3 4 5
-// // start=1, end=N
-// void ShowNumbers(int start, int end)
-// {
-//     if (start == end)  // 1 2 3 4 5
+//     if (J == 0)
 //     {
-//         Console.Write(start);
+//         // Базовый случай - случай остановки рекурсии
+//         Console.Write($"{array[J]}");
 //         return;
 //     }
-//     Console.Write(start + " ");// "привет" + "2" = "привет2"
-//     ShowNumbers(start + 1, end);
+//     Console.Write($"{array[J]}");
+//     PrintInvertedArray(array, J - 1);
 // }
-// ShowNumbers(1, 5); // start = 1, end = 5
+
+// Console.WriteLine("Программа отображает введенные элементы массива с конца с помощью рекурсии.");
+// Console.WriteLine();
+
+// Console.Write("Введите элементы массива:");
+// string line = Console.ReadLine();
+// char[] array = line.ToCharArray();
+// int J = array.Length - 1;
+// PrintInvertedArray(array, J);
